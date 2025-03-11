dd9e1112c157aaa4e8eb2bb3d4c1b3f7e8ea444d
abhayrana001 <abhayrana0290@gmail.com>
2025-03-11 18:55:15 +0530

    rock paper scissor project added

---
+++ 05-rock-paper-scissors/script.js
@@ -0,0 +1,17 @@
+function play(userChoice) {
+    let choices = ["rock", "paper", "scissors"];
+    let computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
+    if (userChoice === computerChoice) {
+        result = "It's a tie!";
+    } else if (
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "rock") ||
+        (userChoice === "scissors" && computerChoice === "paper")
+    ) {
+        result = "You win!";
+    } else {
+        result = "You lose!";
+    }
+    document.getElementById("result").textContent = `Computer chose ${computerChoice}. ${result}`;
+}
