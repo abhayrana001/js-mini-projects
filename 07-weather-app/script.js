316ca1500ab668ab9b9cbe9a4cf75ab3939ac409
abhayrana001 <abhayrana0290@gmail.com>
2025-04-11 12:26:04 +0530

    modify style

--- 07-weather-app/script.js
+++ 07-weather-app/script.js
@@ -1,13 +1,18 @@
 async function getWeather() {
     const city = document.getElementById("city").value;
-    const apiKey = "bdc2646d376ae5582a4c88f1b9d5f29a";
+    const apiKey = "7b347581703bb5bc477dab39ffd8166f";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    const weatherBox = document.getElementById("weatherResult");
     if (data.main) {
-        document.getElementById("weatherResult").textContent = `Temperature: ${data.main.temp}°C`;
+        weatherBox.innerHTML = `
+                       <h2>${data.name}</h2>
+                       <p>Temperature: ${data.main.temp}°C</p>
+                       <p>Humidity: ${data.main.humidity}%</p>
+                       <p>Weather: ${data.weather[0].description}</p>
+                   `;
     } else {
-        document.getElementById("weatherResult").textContent = "City not found";
+        weatherBox.textContent = "City not found. Please try again.";
     }
 }
